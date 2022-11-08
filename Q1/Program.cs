599ee8777d13a6dafce0886bf50f69f9d8dabcde
Tony <tunglin.yang1@gmail.com>
2022-11-09 00:33:59 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool result1 = 99.IsEven();
+            Console.WriteLine(result1);
+            result1 = 98.IsEven();
+            Console.WriteLine(result1);
+
+            bool result2 =99.IsOdd();
+            Console.WriteLine(result2);
+            result2 = 98.IsOdd();
+            Console.WriteLine(result2);
+
+        }
+    }
+    public static class MyMath
+    {
+        public static bool IsOdd(this int source) { return source % 2 == 1; }
+        public static bool IsEven(this int source) { return source % 2 == 0; }
+
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "5245278";
+
+            string value2 = "as";
+            string value3=string.Empty;
+
+            string result4 = value.Left(999);
+            Console.WriteLine(result4);
+            result4 = value2.Left(1);
+            Console.WriteLine(result4);
+            result4 = value3.Left(2);
+            Console.WriteLine(result4);
+            result4 = value2.Left(-999);
+            Console.WriteLine(result4);
+        }
+    }
+    public static class StringLenngth
+    {
+        public static string Left(this string source, int length)
+        {
+            if (string.IsNullOrEmpty(source)) return string.Empty;
+            if (length <= 0) return string.Empty;
+            if (source.Length <= length) return source;
+
+            return source.Substring(0, length);
+        }
+    }
+}
