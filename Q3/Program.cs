fcfe93d8f5f6857c58b5829ac7c4368e40d1c3b7
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 11:35:01 +0800

    fix

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -8,8 +8,27 @@ namespace Q3
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+		static void Main(string[] args)
+		{
+
+			string value = "1234";
+			int value2 = 10000;
+			var result =value.ToInt(-1);
+			Console.WriteLine(result);
+			value = "123asd";
+			result = value.ToInt(-1);
+			Console.WriteLine(result);			
+		}
     }
+	public static class convert
+	{
+		public static int ToInt(this string value, int defaultvalue)
+		{
+			if(Int32.TryParse(value, out int result))
+			{
+				return result;
+			}
+			else return defaultvalue;
+		}
+	}
 }
